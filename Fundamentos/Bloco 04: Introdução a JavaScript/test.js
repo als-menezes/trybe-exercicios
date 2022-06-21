02d7323bde4941c7af6129866dfb6e7f464fa3cf
als-menezes <als.menezes2@gmail.com>
2022-06-21 15:46:37 -0300

    brincando com varáveis

---
+++ Fundamentos/Bloco 04: Introdução a JavaScript/test.js
@@ -0,0 +1,12 @@
+const myName = "Andre";
+const birthCity = "Vitória de Santo Antão";
+let birthYear = 1992;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "Recife";
+console.log(birthCity);
